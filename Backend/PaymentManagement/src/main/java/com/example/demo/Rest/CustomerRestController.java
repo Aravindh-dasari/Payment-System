29a17fd8dafe9e7bf2255c884ea4021b6a012d1f
Aravindh-dasari <dasariaravindh21@outlook.com>
2022-08-25 17:54:07 +0000

    Updated Customer Rest Controller

--- Backend/PaymentManagement/src/main/java/com/example/demo/Rest/CustomerRestController.java
+++ Backend/PaymentManagement/src/main/java/com/example/demo/Rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
+
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 public class CustomerRestController {
@@ -25,7 +28,7 @@ public class CustomerRestController {
 		return arr;
 	}
 	
-	@GetMapping("/customers/")
+	@GetMapping("/customers")
 	public List<Customer> customers(){
 		
 		return service.AllCustomers();
