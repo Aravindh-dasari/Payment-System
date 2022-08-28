81623b053504794d20713a5f755db2e4c1fca2df
Aravindh-dasari <dasariaravindh21@outlook.com>
2022-08-28 12:44:40 +0000

    Added Message API

---
+++ Backend/Databases/Data/Message.sql
@@ -0,0 +1,9 @@
+INSERT INTO message(messagecode,instruction) VALUES ('CHQB','beneficiary customer must be paid by cheque only.');
+INSERT INTO message(messagecode,instruction) VALUES ('CORT','Payment is made in settlement for a trade.');
+INSERT INTO message(messagecode,instruction) VALUES ('HOLD','Beneficiary customer or claimant will call upon identification.');
+INSERT INTO message(messagecode,instruction) VALUES ('INTC','Payment between two companies that belongs to the same group.');
+INSERT INTO message(messagecode,instruction) VALUES ('PHOB','Please advise the intermediary institution by phone.');
+INSERT INTO message(messagecode,instruction) VALUES ('PHOI','Please advise the intermediary by phone.');
+INSERT INTO message(messagecode,instruction) VALUES ('PHON','Please advise the account with institution by phone.');
+INSERT INTO message(messagecode,instruction) VALUES ('REPA','Payments has a related e-Payments reference.');
+INSERT INTO message(messagecode,instruction) VALUES ('SDVA','Payment must be executed with same day value to the');
